41b41960fd77880a54435e528ff056975822125f
suruthisanju <47382342+suruthisanju@users.noreply.github.com>
2019-02-06 15:04:04 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,29 @@
+import java.io*;
+public class GUVI
+{
+public static void main(String[])
+{
+ 
+int n=3;
+
+ if(n==3)
+ 
+{
+
+ System.out.println("postive");
+ 
+}   
+ 
+else if(n==-3)
+ 
+{
+  System.out.println("negative");
+ 
+}
+  
+else if(n==0)
+  {
+  system.out.println("zero");
+  }
+  }
+  }
